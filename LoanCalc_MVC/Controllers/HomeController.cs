44d23645a91e36261693d45d4561ce4021aa69c0
TSZI <troy.m.szatkowski@gmail.com>
2023-06-11 16:54:54 -0400

    Add project files.

---
+++ LoanCalc_MVC/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using LoanCalc_MVC.Helpers;
+using LoanCalc_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoanCalc_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult LoanPage()
+        {
+            Loan model = new Loan();
+
+            model.Payment = 0;
+            model.TotalInterest = 0;
+            model.TotalCost = 0;
+            model.Rate = 3.5M;
+            model.Amount = 15000M;
+            model.Term = 60;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult LoanPage(Loan model)
+        {
+            LoanHelper loanHelper = new LoanHelper();
+
+            Loan newLoan = loanHelper.GetPayments(model);
+
+            return View(newLoan);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
